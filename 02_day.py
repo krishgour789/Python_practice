c28ef2f8c1cb25a768776ebee244b3dfd06942c6
krishgour789 <krishgour789@gmail.com>
2025-12-16 10:17:52 +0530

    16/12/25

---
+++ 02_day.py
@@ -0,0 +1,7 @@
+# ❌ “Why does this code give ERROR?”
+a = 5
+if a==5:
+    print("hello world")
+
+# = => assisgnment
+# == > Comparison 
---
+++ 03_Day.py
@@ -0,0 +1,7 @@
+'''Differnce bt append or  extend'''
+a = [1,2]
+# a.append([3,4])
+# print(a)
+
+a.extend([3,4])
+print(a)
