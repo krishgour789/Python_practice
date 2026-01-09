0b79c91267d56dbe711af97d3752b05b863210e6
krishgour789 <krishgour789@gmail.com>
2026-01-09 13:47:13 +0530

    9/12/25

--- 10day.py
+++ 10day.py
@@ -1,1 +1,18 @@
-print(10+20*2)
+# print("hello world")
+# print(774734)
+
+# Variable
+
+# name = "Krish Gour"
+# age = 21
+# print(name)
+# print(age)
+
+name = "Krish Gour"
+age = 21
+price = 77.445
+active = True
+print(type(name))
+print(type(age))
+print(type(price))
+print(type(active))
