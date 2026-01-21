89604db509255530eeea24644536705d62a80177
krishgour789 <krishgour789@gmail.com>
2026-01-21 23:24:12 +0530

    21/01/25

--- 10day.py
+++ 10day.py
@@ -8,11 +8,41 @@
 # print(name)
 # print(age)
 
-name = "Krish Gour"
-age = 21
-price = 77.445
-active = True
-print(type(name))
-print(type(age))
-print(type(price))
-print(type(active))
+# name = "Krish Gour"
+# age = 21
+# price = 77.445
+# active = True
+# print(type(name))
+# print(type(age))
+# print(type(price))
+# print(type(active))
+
+
+# second larges
+# li = [3,4,2,6,10,7]
+# max = li[0]
+# sec = li[0]
+# for i in li:
+#     if i>max:
+#         sec = max
+#         max = i
+#     if i>sec and i<max:
+#         sec = i
+#     print(sec)
+
+# li = [3,4,2,6,10,7]
+# max = li[0]
+# for i in li:
+#     if i>max:
+#         max = i
+# print(max)
+
+
+
+
+num = 7
+print(num*9%10)
+
+
+
+
